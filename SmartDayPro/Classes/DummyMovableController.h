d1e40cf51aae6b1d1e7dbedc86474ad07a99531d
TrungNguyen <ngthtrung@leftcoastlogic.com>
2013-03-20 09:15:54 +0700

    [SD iPad]

---
+++ SmartDayPro/Classes/DummyMovableController.h
@@ -0,0 +1,13 @@
+//
+//  DummyMovableController.h
+//  SmartDayPro
+//
+//  Created by Left Coast Logic on 3/18/13.
+//  Copyright (c) 2013 Left Coast Logic. All rights reserved.
+//
+
+#import "MovableController.h"
+
+@interface DummyMovableController : MovableController
+
+@end
---
+++ SmartDayPro/Classes/DummyMovableController.m
@@ -0,0 +1,13 @@
+//
+//  DummyMovableController.m
+//  SmartDayPro
+//
+//  Created by Left Coast Logic on 3/18/13.
+//  Copyright (c) 2013 Left Coast Logic. All rights reserved.
+//
+
+#import "DummyMovableController.h"
+
+@implementation DummyMovableController
+
+@end
