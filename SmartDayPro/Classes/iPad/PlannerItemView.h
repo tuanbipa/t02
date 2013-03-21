dd278cbd5e4a8b6862ecb1c371b5f81eb5ae58b5
TrungNguyen <ngthtrung@leftcoastlogic.com>
2013-03-21 10:27:58 +0700

    [SD iPad]

---
+++ SmartDayPro/Classes/iPad/PlannerItemView.h
@@ -0,0 +1,13 @@
+//
+//  PlannerItemView.h
+//  SmartDayPro
+//
+//  Created by Nguyen Van Thuc on 3/21/13.
+//  Copyright (c) 2013 Left Coast Logic. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface PlannerItemView : UIView
+
+@end
