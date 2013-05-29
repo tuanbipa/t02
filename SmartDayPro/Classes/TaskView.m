094d31ed43276d9e991c8357f2f609f9b1c68f4b
Thuc <thucwiki@gmail.com>
2013-05-29 13:16:20 +0700

    [Manual Task] draw dash border

--- SmartDayPro/Classes/TaskView.m
+++ SmartDayPro/Classes/TaskView.m
@@ -1095,6 +1095,14 @@ extern PlannerViewController *_plannerViewCtrler;
         
         CGContextStrokeRect(ctx, rect);
     }
+    
+    if ([task isManual]) {
+        // draw dash border
+        //CGRect dashRect = CGRectInset(rect, 1, 1);
+        CGFloat dashPattern[]= {4.0, 3};
+        CGContextSetLineDash(ctx, 0.0, dashPattern, 2);
+        strokeRoundedRect(ctx, rect, 5, 5);
+    }
 
     /*
     UIColor *separatorColor = [UIColor colorWithRed:243.0/255 green:243.0/255 blue:243.0/255 alpha:1];
