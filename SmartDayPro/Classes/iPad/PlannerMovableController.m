25a701e4e4f9d6bc8d4e776d223f479728ee0097
Thuc <thucwiki@gmail.com>
2013-05-30 13:16:47 +0700

    [Planner] all supper end move after update event

--- SmartDayPro/Classes/iPad/PlannerMovableController.m
+++ SmartDayPro/Classes/iPad/PlannerMovableController.m
@@ -207,6 +207,7 @@ extern PlannerViewController *_plannerViewCtrler;
     
     if ([task isEvent]) { // change evnt datetime
         [self changeEventDateTime:task];
+        [super endMove:self.activeMovableView];
     } else {
         // convert task to STask
         UIAlertView *alertView = [[UIAlertView alloc] initWithTitle:_warningText  message:_convertIntoSTaskConfirmation delegate:self cancelButtonTitle:_cancelText otherButtonTitles:_okText, nil];
