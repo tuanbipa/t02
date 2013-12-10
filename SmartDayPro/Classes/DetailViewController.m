f459e280de5bafa96b446496531c6c5f39a7a8b0
Thuc <thucwiki@gmail.com>
2013-12-10 17:10:53 +0700

    [Timezone Support][DetailViewController] remove DST time

--- SmartDayPro/Classes/DetailViewController.m
+++ SmartDayPro/Classes/DetailViewController.m
@@ -1378,7 +1378,7 @@ DetailViewController *_detailViewCtrler = nil;
         //NSInteger secs = [Common getSecondsFromTimeZoneID:self.taskCopy.timeZoneId] - [[NSTimeZone defaultTimeZone] secondsFromGMT];
         
         // sub DST offset
-        NSInteger secs = 2*[Common getSecondsFromTimeZoneID:self.taskCopy.timeZoneId] - [[NSTimeZone defaultTimeZone] secondsFromGMT] - [[Settings getTimeZoneByID:self.taskCopy.timeZoneId] secondsFromGMT];
+        NSInteger secs = [[NSTimeZone defaultTimeZone] secondsFromGMT] - [[Settings getTimeZoneByID:self.taskCopy.timeZoneId] secondsFromGMT];
         
         startTime = [startTime dateByAddingTimeInterval:secs];
         endTime = [endTime dateByAddingTimeInterval:secs];
