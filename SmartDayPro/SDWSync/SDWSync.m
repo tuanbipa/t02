a767fb2331c052177d25e2c0ce4c12c25873d990
Thuc <thucwiki@gmail.com>
2013-12-13 16:42:00 +0700

    [Smart Share][Meeting Invite] owner: show as normal; member: if MI was accepted, show as normal else show as transparent

--- SmartDayPro/SDWSync/SDWSync.m
+++ SmartDayPro/SDWSync/SDWSync.m
@@ -1577,10 +1577,14 @@ NSInteger _sdwColor[32] = {
     //ret.extraStatus = [[dict objectForKey:@"shared"] intValue];
     [ret setShared:[[dict objectForKey:@"shared"] intValue]];
     
-    [ret setMeetingInvited:[[dict objectForKey:@"meeting_invite_flag"] intValue]];
+    //[ret setMeetingInvited:[[dict objectForKey:@"meeting_invite_flag"] intValue]];
     
     if (![ret isShared]) {
+        // shared = 0 and delegated = 1: it mean this task was accepted by assignee
         [ret setAcceptedStatus:([[dict objectForKey:@"delegate_status"] intValue] == 1)]; // 1 mean accepted
+    } else {
+        // shared = 1 and meeting_invite_flag = 1: it mean this MI was accepted by this member
+        [ret setMeetingInvited:[[dict objectForKey:@"meeting_invite_flag"] intValue]];
     }
     
     ret.assigneeEmail = [self getStringValue:@"assignee_email" dict:dict];
